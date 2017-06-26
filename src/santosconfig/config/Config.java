734d56b7466058f8ac376b5d0415f9ff57e85a87
justdasc <justdasc@WC-2FJ4SY1TL.performance.pfgc.com>
2017-06-26 15:11:07 -0400

    Removed unused imports.

--- src/santosconfig/config/Config.java
+++ src/santosconfig/config/Config.java
@@ -5,17 +5,13 @@
  */
 package santosconfig.config;
 
-import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.OutputStream;
-import java.util.HashMap;
 import java.util.Properties;
-import java.util.logging.Level;
-import java.util.logging.Logger;
 import static santosconfig.SantosConfig.l;
 import santosutils.file.FileManager;
 
